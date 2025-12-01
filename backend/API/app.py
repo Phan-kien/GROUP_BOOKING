0f0e5e037ee5e254c34173158b30e3d4fae811e2
Phan Huy Kiên <kien.phan2202091@std.ttu.edu.vn>
2025-12-01 21:12:57 +0700

    kien1

--- backend/API/app.py
+++ backend/API/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
 from backend.API.auth import auth_api
+from backend.API.book import book_api
 from backend.API.cinema import cinema_api
-from backend.API.movie import movie_api
-from backend.API.showtime import showtime_api
 from backend.API.combo import combo_api
+from backend.API.hall import hall_api
+from backend.API.movie import movie_api
+from backend.API.payment import payment_api
+from backend.API.showtime import showtime_api
 from backend.API.order import order_api
 from backend.API.ticket import ticket_api
 
@@ -12,12 +15,15 @@ app = Flask(__name__)
 
 # register tất cả API
 app.register_blueprint(auth_api, url_prefix="/api")
+app.register_blueprint(hall_api, url_prefix="/api")
+app.register_blueprint(book_api, url_prefix="/api")
 app.register_blueprint(cinema_api, url_prefix="/api")
 app.register_blueprint(movie_api, url_prefix="/api")
 app.register_blueprint(showtime_api, url_prefix="/api")
 app.register_blueprint(combo_api, url_prefix="/api")
 app.register_blueprint(order_api, url_prefix="/api")
 app.register_blueprint(ticket_api, url_prefix="/api")
+app.register_blueprint(payment_api, url_prefix="/api")
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port=5000)
